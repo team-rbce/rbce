c272f724fc330e6159e63893a25c513aa13dc7da
Samuel Deotti <samuel_deotti@hotmail.com>
2024-07-24 17:04:14 -0300

    fixing header and footer links

--- src/components/Footer/FooterNav.jsx
+++ src/components/Footer/FooterNav.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function FooterNav() {
   return (
     <div className="flex flex-wrap flex-col items-center justify-center w-full sm:flex-row sm:gap-4 sm:justify-between md:w-fit">
-      <p className="font-bold italic text-3xl tracking-tighter sm:font-extrabold ">rbce</p>
+      <p className="font-bold italic text-3xl tracking-tighter sm:font-extrabold "><Link to="/">rbce</Link></p>
       <nav className="flex flex-col justify-center items-center gap-2 my-2 sm:flex-row sm:gap-4">
         <NavLink to="nossos-projetos" className="hover:text-color-tertiary">projetos</NavLink>
         <NavLink to="quem-somos" className="hover:text-color-tertiary">quem somos</NavLink>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderMenu from './HeaderMenu';
 import HeaderNav from './HeaderNav';
 import Logo from '../Logo/Logo';
@@ -8,7 +9,7 @@ export default function Header() {
     <header className="flex px-3 py-4 justify-between items-center bg-background-primary-light sm:px-10 md:px-16 lg:px-24">
       <div className="flex items-center gap-2 text-[#3c4c41]">
         <Logo />
-        <h1 className="font-LibreFranklinItalic font-black text-4xl tracking-tighter">rbce</h1>
+        <h1 className="font-LibreFranklinItalic font-black text-4xl tracking-tighter"><Link to="/">rbce</Link></h1>
       </div>
       <div className="font-LibreFranklin text-background-primary-dark">
         <HeaderMenu />
