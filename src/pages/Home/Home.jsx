31d9d543939e9f3cb3bc31b0255d99dbaf926b04
Samuel Deotti <samuel_deotti@hotmail.com>
2024-08-04 21:12:41 -0300

    data grid

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,7 +14,28 @@ export default function Home() {
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia rerum sequi facilis vitae fuga corrupti! Blanditiis veritatis earum eius aliquam quae repudiandae dignissimos, laborum, commodi qui ipsam cumque nesciunt dolore error asperiores rerum aut eum illo explicabo eos! Quod culpa id quas mollitia iure qui magnam veritatis laboriosam sit adipisci. Illo suscipit consequatur animi accusamus, voluptatum cumque quam aliquam magnam veritatis labore sit? Ullam veniam, eaque expedita ducimus cum quos!
           </p>
         </div>
-        <Title>Alguns dados importantes</Title>
+        <div className="mb-20">
+          <Title>Alguns dados importantes</Title>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 font-Inter">
+            <div className="ring-1 ring-color-tertiary p-4 rounded-lg">
+              <h3 className="text-lg font-Inter font-bold">1.000</h3>
+              <p className="text-base font-Inter">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, repudiandae!</p>
+            </div>
+            <div className="ring-1 ring-color-tertiary p-4 rounded-lg">
+              <h3 className="text-lg font-Inter font-bold">1.000</h3>
+              <p className="text-base font-Inter">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, repudiandae!</p>
+            </div>
+            <div className="ring-1 ring-color-tertiary p-4 rounded-lg">
+              <h3 className="text-lg font-Inter font-bold">1.000</h3>
+              <p className="text-base font-Inter">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, repudiandae!</p>
+            </div>
+            <div className="ring-1 ring-color-tertiary p-4 rounded-lg">
+              <h3 className="text-lg font-Inter font-bold">1.000</h3>
+              <p className="text-base font-Inter">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, repudiandae!</p>
+            </div>
+          </div>
+
+        </div>
         <Title>Principais projetos</Title>
         <Title>Parceiros</Title>
         <Title>Redes sociais</Title>
