9f73e629d1dfa5f70cf2afea7461f1eb8fed45ed
Samuel Deotti <samuel_deotti@hotmail.com>
2024-08-07 13:47:06 -0300

    refactoring cards

--- src/pages/Developers/Developers.jsx
+++ src/pages/Developers/Developers.jsx
@@ -3,14 +3,44 @@ import Title from '../../components/Title/Title';
 import DevelopersCard from './DevelopersCard';
 
 export default function Developers() {
+  const developers = [
+    {
+      imagePath: '/defaultAvatar.jpeg',
+      /* imagePath: '/anaNunesAvatar.jpeg', */
+      name: 'Ana Beatriz Nunes',
+      description: 'Desenvolvedora e Monitora Fullstack, foi responsável por idear o design do site, desenvolver o style guide e fazer o rebranding da RBCE.',
+      socials: ['linkedin', 'github'],
+    },
+    {
+      imagePath: '/defaultAvatar.jpeg',
+      /* imagePath: '/FilipeAvatar.jpeg', */
+      name: 'Filipe',
+      description: 'Desenvolvedor Backend e profissional de Enfermagem, foi responsável por desenvolver o site da RBCE, recuperar dados e auxiliar na mídia do site.',
+      socials: ['linkedin', 'github'],
+    },
+    {
+      imagePath: '/defaultAvatar.jpeg',
+      /* imagePath: '/samuelAvatar.jpeg', */
+      name: 'Samuel Deotti',
+      description: 'Desenvolvedor Fullstack, foi responsável por desenvolver o site da RBCE e auxiliar no contexto final para a produção.',
+      socials: ['https://br.linkedin.com/in/samuel-deotti', 'https://github.com/samueldeotti'],
+    },
+    {
+      imagePath: '/defaultAvatar.jpeg',
+      /* imagePath: '/yanAvatar.jpeg', */
+      name: 'Yan',
+      description: 'Desenvolvedor Frontend, foi responsável por idear o design, desenvolver o site da RBCE, auxiliar na mídia do site.',
+      socials: ['linkedin', 'github'],
+    },
+  ];
+
   return (
     <div className="w-full h-full px-3 py-4 sm:px-10 md:px-16 lg:px-24">
       <Title>Time de desenvolvimento</Title>
-      <div className="flex flex-col gap-6">
-        <DevelopersCard name="Ana Beatriz Nunes" description="Desenvolvedora e Monitora Fullstack, foi responsável por idear o design do site, desenvolver o style guide e fazer o rebranding da RBCE." socials={ ['linkedin', 'github'] } />
-        <DevelopersCard name="Filipe" description="Desenvolvedor Backend e profissional de Enfermagem, foi responsável por desenvolver o site da RBCE, recuperar dados e auxiliar na mídia do site." socials={ ['linkedin', 'github'] } />
-        <DevelopersCard name="Samuel" description="Desenvolvedor Fullstack, foi responsável por desenvolver o site da RBCE e auxiliar no contexto final para a produção." socials={ ['https://br.linkedin.com/in/samuel-deotti', 'https://github.com/samueldeotti'] } />
-        <DevelopersCard name="Yan" description="Desenvolvedor Frontend, foi responsável por idear o design, desenvolver o site da RBCE, auxiliar na mídia do site." socials={ ['linkedin', 'github'] } />
+      <div className="flex flex-col gap-8 my-12">
+
+        {developers.map((developer, index) => <DevelopersCard key={ developer.name + index } { ...developer } />)}
+
       </div>
     </div>
   );
--- src/pages/Developers/DevelopersCard.jsx
+++ src/pages/Developers/DevelopersCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function DevelopersCard({ name, description, socials }) {
+export default function DevelopersCard({ imagePath, name, description, socials }) {
   return (
-    <div className="flex flex-col items-center font-Inter bg-white rounded-2xl p-4 gap-4 shadow-md my-6">
-      <img src="/defaultAvatar.jpeg" alt="" className="rounded-full h-20 w-20" />
+    <div style={ { boxShadow: 'rgba(0, 0, 0, 0.301) 0px 4px 12px' } } className="flex flex-col items-center font-Inter bg-white rounded-2xl p-4 gap-4">
+      <img src={ imagePath } alt={ `${name}avatar` } className="rounded-full h-20 w-20" />
       <h3 className="font-semibold text-xl text-background-primary-dark">{name}</h3>
       <p className="text-[#787878] text-center">{description}</p>
       <div className="flex">
@@ -22,4 +22,5 @@ DevelopersCard.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   socials: PropTypes.arrayOf(PropTypes.string).isRequired,
+  imagePath: PropTypes.string.isRequired,
 };
