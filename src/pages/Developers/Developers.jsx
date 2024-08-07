95d369592c242139d94a3bf23cf32239f55ef8a1
Samuel Deotti <samuel_deotti@hotmail.com>
2024-08-07 13:46:39 -0300

    mobile style

--- src/pages/Developers/Developers.jsx
+++ src/pages/Developers/Developers.jsx
@@ -9,7 +9,7 @@ export default function Developers() {
       <div className="flex flex-col gap-6">
         <DevelopersCard name="Ana Beatriz Nunes" description="Desenvolvedora e Monitora Fullstack, foi responsável por idear o design do site, desenvolver o style guide e fazer o rebranding da RBCE." socials={ ['linkedin', 'github'] } />
         <DevelopersCard name="Filipe" description="Desenvolvedor Backend e profissional de Enfermagem, foi responsável por desenvolver o site da RBCE, recuperar dados e auxiliar na mídia do site." socials={ ['linkedin', 'github'] } />
-        <DevelopersCard name="Samuel" description="Desenvolvedor Fullstack, foi responsável por desenvolver o site da RBCE e auxiliar no contexto final para a produção." socials={ ['linkedin', 'github'] } />
+        <DevelopersCard name="Samuel" description="Desenvolvedor Fullstack, foi responsável por desenvolver o site da RBCE e auxiliar no contexto final para a produção." socials={ ['https://br.linkedin.com/in/samuel-deotti', 'https://github.com/samueldeotti'] } />
         <DevelopersCard name="Yan" description="Desenvolvedor Frontend, foi responsável por idear o design, desenvolver o site da RBCE, auxiliar na mídia do site." socials={ ['linkedin', 'github'] } />
       </div>
     </div>
--- src/pages/Developers/DevelopersCard.jsx
+++ src/pages/Developers/DevelopersCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function DevelopersCard({ name, description, socials }) {
   return (
-    <div className="flex flex-col items-center font-Inter bg-gray-100 rounded-2xl p-4 gap-4">
+    <div className="flex flex-col items-center font-Inter bg-white rounded-2xl p-4 gap-4 shadow-md my-6">
       <img src="/defaultAvatar.jpeg" alt="" className="rounded-full h-20 w-20" />
       <h3 className="font-semibold text-xl text-background-primary-dark">{name}</h3>
       <p className="text-[#787878] text-center">{description}</p>
