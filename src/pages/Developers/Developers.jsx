cef5d7dc248baead8e06aeb38c3e0ac1babec552
Samuel Deotti <samuel_deotti@hotmail.com>
2024-08-07 11:45:34 -0300

    developers card style

--- src/pages/Developers/Developers.jsx
+++ src/pages/Developers/Developers.jsx
@@ -6,8 +6,11 @@ export default function Developers() {
   return (
     <div className="w-full h-full px-3 py-4 sm:px-10 md:px-16 lg:px-24">
       <Title>Time de desenvolvimento</Title>
-      <div>
-        <DevelopersCard name="" description="" socials={ ['', ''] } />
+      <div className="flex flex-col gap-6">
+        <DevelopersCard name="Ana Beatriz Nunes" description="Desenvolvedora e Monitora Fullstack, foi responsável por idear o design do site, desenvolver o style guide e fazer o rebranding da RBCE." socials={ ['linkedin', 'github'] } />
+        <DevelopersCard name="Filipe" description="Desenvolvedor Backend e profissional de Enfermagem, foi responsável por desenvolver o site da RBCE, recuperar dados e auxiliar na mídia do site." socials={ ['linkedin', 'github'] } />
+        <DevelopersCard name="Samuel" description="Desenvolvedor Fullstack, foi responsável por desenvolver o site da RBCE e auxiliar no contexto final para a produção." socials={ ['linkedin', 'github'] } />
+        <DevelopersCard name="Yan" description="Desenvolvedor Frontend, foi responsável por idear o design, desenvolver o site da RBCE, auxiliar na mídia do site." socials={ ['linkedin', 'github'] } />
       </div>
     </div>
   );
--- src/pages/Developers/DevelopersCard.jsx
+++ src/pages/Developers/DevelopersCard.jsx
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 
 export default function DevelopersCard({ name, description, socials }) {
   return (
-    <div>
-      <img src="" alt="" />
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <div>
+    <div className="flex flex-col items-center font-Inter bg-gray-100 rounded-2xl p-4 gap-4">
+      <img src="/defaultAvatar.jpeg" alt="" className="rounded-full h-20 w-20" />
+      <h3 className="font-semibold text-xl text-background-primary-dark">{name}</h3>
+      <p className="text-[#787878] text-center">{description}</p>
+      <div className="flex">
         {socials.map((social) => (
           <a href={ social } key={ social } target="_blank" aria-label="Social Link">
-            <img src="" alt="" />
+            <img src="/linkedin.png" alt="" />
           </a>
         ))}
       </div>
