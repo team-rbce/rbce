1052ed67bd87d39f02d116d7c11ce54f6baefdb7
Samuel Deotti <samuel_deotti@hotmail.com>
2024-08-08 10:13:04 -0300

    fixing problem with the main style

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
   return (
     <>
       <Header />
-      <main className="">
+      <main className="flex-1">
         <Outlet />
       </main>
       {/* Esta quebrado por enquanto */}
