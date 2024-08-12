c62799554ca676e484e7229468a6081325ead8a4
Samuel Deotti <samuel_deotti@hotmail.com>
2024-08-12 15:23:23 -0300

    fixing contact button style

--- src/pages/Layout/Header/HeaderNavMenu.jsx
+++ src/pages/Layout/Header/HeaderNavMenu.jsx
@@ -9,7 +9,7 @@ export default function HeaderNavMenu({ isMenuOpen }) {
   return (
     <nav className={ `${isMenuOpen ? 'block' : 'hidden'} sm:hidden absolute z-10 bg-background-primary-light w-full flex flex-col gap-4 text-xl justify-center px-4 pt-2 pb-6 font-Inter font-semibold shadow-bottom-only top-20` }>
       <HeaderNavItem>projetos</HeaderNavItem>
-      <button onClick={ () => setIsOpen(!isOpen) } aria-label="Toggle Email Modal" className="self-start hover:text-color-tertiary">fale conosco</button>
+      <button onClick={ () => setIsOpen(!isOpen) } aria-label="Toggle Email Modal" className="hover:text-color-tertiary w-full text-left">fale conosco</button>
       <HeaderNavItem>quem somos</HeaderNavItem>
       <HeaderNavItem>nossa trajetoria</HeaderNavItem>
       {isOpen && <EmailModal setIsModalOpen={ setIsOpen } />}
